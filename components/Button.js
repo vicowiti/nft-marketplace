3b96a7b12ff75748d616b32cb5f0cf2207ebc40e
Victor Owiti <viowiti12@gmail.com>
2022-06-05 14:18:19 +0300

    added the homeheader ui, pending search functionality

--- components/Button.js
+++ components/Button.js
@@ -1,18 +1,54 @@
 import { TouchableOpacity, Image, View, Text } from "react-native";
 import React from "react";
+import { COLORS, FONTS, SHADOWS, SIZES } from "../constants";
 
-export const CircleButton = () => {
+export const CircleButton = ({ imgUrl, handlePress, ...props }) => {
   return (
-    <TouchableOpacity>
-      <Text>Button</Text>
+    <TouchableOpacity
+      style={{
+        width: 40,
+        height: 40,
+        backgroundColor: COLORS.white,
+        position: "absolute",
+        borderRadius: SIZES.extraLarge,
+        alignItems: "center",
+        justifyContent: "center",
+        ...SHADOWS.light,
+        ...props,
+      }}
+      onPress={handlePress}
+    >
+      <Image
+        source={imgUrl}
+        resizeMode="contain"
+        style={{ width: 24, height: 24 }}
+      />
     </TouchableOpacity>
   );
 };
 
-export const RectButton = () => {
+export const RectButton = ({ minWidth, fontSize, handlePress, ...props }) => {
   return (
-    <View>
-      <Text>Button</Text>
-    </View>
+    <TouchableOpacity
+      style={{
+        backgroundColor: COLORS.primary,
+        borderRadius: SIZES.extraLarge,
+        maxWidth: minWidth,
+        padding: SIZES.small,
+        ...props,
+      }}
+      onPress={handlePress}
+    >
+      <Text
+        style={{
+          fontFamily: FONTS.semiBold,
+          color: COLORS.white,
+          fontSize: fontSize,
+          textAlign: "center",
+        }}
+      >
+        Place a bid
+      </Text>
+    </TouchableOpacity>
   );
 };
--- components/NTFCard.js
+++ components/NTFCard.js
@@ -3,6 +3,7 @@ import React from "react";
 import { useNavigation } from "@react-navigation/native";
 import { COLORS, SHADOWS, SIZES, assets } from "../constants";
 import { CircleButton, RectButton } from "./Button";
+import { SubInfo, EthPrice, NFTTitle } from "./SubInfo";
 
 const NTFCard = ({ data }) => {
   const navigation = useNavigation();
@@ -30,7 +31,35 @@ const NTFCard = ({ data }) => {
 
         <CircleButton imgUrl={assets.heart} right={10} top={10} />
       </View>
-      <Text>NTFCard</Text>
+      <SubInfo />
+      <View
+        style={{
+          width: "100%",
+          padding: SIZES.font,
+        }}
+      >
+        <NFTTitle
+          title={data.name}
+          subTitle={data.creator}
+          titleSize={SIZES.large}
+          subTitleSize={SIZES.small}
+        />
+        <View
+          style={{
+            marginTop: SIZES.font,
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <EthPrice price={data.price} />
+          <RectButton
+            minWidth={120}
+            fontSize={SIZES.font}
+            handlePress={() => navigation.navigate("Details", { data })}
+          />
+        </View>
+      </View>
     </View>
   );
 };
