369daedca817b43eb355e3fc1cc4c8958512d01d
Victor Owiti <viowiti12@gmail.com>
2022-06-06 20:03:48 +0300

    eas config

--- screens/Details.js
+++ screens/Details.js
@@ -45,7 +45,7 @@ const DetailsHeader = ({ data, navigation }) => {
 
 const Details = ({ route, navigation }) => {
   const { data } = route.params;
-  console.log(data);
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <FocusedStatusBar
